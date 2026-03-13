4d37f4b14977b60f0cb880fe9406452d18e5497b
Ethan H. Nguyen <ethanhducnguyen@gmail.com>
2026-03-12 20:13:18 -0500

    started chat router

--- backend/controllers/chatController.js
+++ backend/controllers/chatController.js
@@ -1,6 +1,20 @@
 import dotenv from "dotenv"
+import readline from "readline"
 import { GoogleGenerativeAI } from "@google/genai"
 
-dotenv.config();
+dotenv.config(); // Load environment variables from .env file
 
 const genAI = new GoogleGenerativeAI(process.env.AI_API_KEY);
+
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+})
+
+async function main(){
+    const model = genAI.getGenerativeModel({model : "Default Gemini API Key"})
+}
+
+module.exports = {
+  
+}
---
+++ backend/routes/chat.js
@@ -0,0 +1,10 @@
+const express = require('express') // import express module to create router
+const {
+
+} = require('../controllers/chatController')
+
+const router = express.Router()
+
+router.post('/',)
+
+module.exports = router
